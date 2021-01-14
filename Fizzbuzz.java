723d8f1285d4ee8e9242220279ba01db816619d4
yus2on <gbwl2784@gmail.com>
2021-01-14 22:30:48 +0900

    feat: Complete Fizzbuzz with if, for
    solved: #3

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -8,12 +8,15 @@ class Solution {
       for (int i = 0; i <= n; i++) {
         if (i % 15 == 0) {
           res.add("FizzBuzz");
+        } else if (i % 5 == 0) {
+          res.add("Buzz");
+        } else if (i % 3 == 0) {
+          res.add("Fizz");
         } else {
           res.add(String.valueOf(i));
         }
-        System.out.println(res);
       } // end for 
-
+      
       return res;
     }
 }
