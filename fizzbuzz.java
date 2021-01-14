3c805ea91dd19d21898efa6cb8e6a85c70440034
LNoah <limnoah0301@gmail.com>
2021-01-14 21:28:12 +0900

    feat: Create fizzbuzz 3

---
+++ fizzbuzz.java
@@ -0,0 +1,11 @@
+public class fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if(i % 3 == 0){
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+   }
+
