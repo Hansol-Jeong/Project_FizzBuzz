86c49159697ab67c32c1a829742f41cd041131de
Tezla-Lee <tezla.lee@gmail.com>
2021-01-14 23:58:09 +0900

    feat: modify fibonaChiken.java
    modify howManyChicken method
    solved: #1

--- src/FibonaChicken.java
+++ src/FibonaChicken.java
@@ -1,36 +1,30 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class FibonaChicken {
     public static void main(String[] args) {
-        System.out.println(howManyChicken(9));
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(howManyChicken(num));
     }
 
     static int howManyChicken(int num) {
-        int[] fibonazzi = new int[num + 1];
-
-        fibonazzi[0] = 0;
-        fibonazzi[1] = 1;
-        fibonazzi[2] = 1;
-
-        int big = 0;
-        int mid = 0;
-        int small = 0;
-
-        for (int i = 3; i < fibonazzi.length; i++) {
-            fibonazzi[i] = fibonazzi[i - 2] + fibonazzi[i - 1];
-            if (fibonazzi[i] == num) {
-                System.out.println(Arrays.toString(fibonazzi));
-                return fibonazzi[i - 1];
-            } else if (fibonazzi[i] > num) {
-                big = fibonazzi[i];
-                mid = fibonazzi[i - 1];
-                small = fibonazzi[i - 2];
-                break;
-            }
+        if (num < 1) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
         }
 
-        System.out.println(Arrays.toString(fibonazzi));
+        int f1 = 0;
+        int f2 = 1;
+        int f3 = 1;
 
-        return small + (mid - small) * (num - mid) / (big - mid);
+        while (f3 < num) {
+            f1 = f2;
+            f2 = f3;
+            f3 = f3 + f1;
+        }
+
+        return f1 + Math.round((float) (f2 - f1) * (num - f2) / (f3 - f2));
     }
 }
