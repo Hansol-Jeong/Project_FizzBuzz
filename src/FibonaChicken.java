9f45f72a6e7bdda971ed84237a022a56821d614a
Tezla-Lee <tezla.lee@gmail.com>
2021-01-14 21:53:00 +0900

    feat: complete FibonaChicken

--- src/FibonaChicken.java
+++ src/FibonaChicken.java
@@ -2,34 +2,35 @@ import java.util.Arrays;
 
 public class FibonaChicken {
     public static void main(String[] args) {
-        howManyChicken(10);
-
+        System.out.println(howManyChicken(9));
     }
 
     static int howManyChicken(int num) {
         int[] fibonazzi = new int[num + 1];
-        int[] fibonaChicken = new int[num + 1];
 
         fibonazzi[0] = 0;
         fibonazzi[1] = 1;
         fibonazzi[2] = 1;
 
+        int big = 0;
+        int mid = 0;
+        int small = 0;
+
         for (int i = 3; i < fibonazzi.length; i++) {
             fibonazzi[i] = fibonazzi[i - 2] + fibonazzi[i - 1];
-        }
-
-        for (int i = 1; fibonazzi[i + 1] <= fibonaChicken.length; i++) {
-            fibonaChicken[fibonazzi[i + 1]] = fibonazzi[i];
-        }
-
-        for (int i = 1; i < fibonaChicken.length; i++) {
-            if (fibonaChicken[i] == 0) {
-
+            if (fibonazzi[i] == num) {
+                System.out.println(Arrays.toString(fibonazzi));
+                return fibonazzi[i - 1];
+            } else if (fibonazzi[i] > num) {
+                big = fibonazzi[i];
+                mid = fibonazzi[i - 1];
+                small = fibonazzi[i - 2];
+                break;
             }
         }
 
         System.out.println(Arrays.toString(fibonazzi));
-        System.out.println(Arrays.toString(fibonaChicken));
-        return fibonazzi[num];
+
+        return small + (mid - small) * (num - mid) / (big - mid);
     }
 }
